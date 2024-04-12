6e6442d17510ce1e1dbfb54bad5fa070a7ff4eca
OmingoEmma <emmaomingo@gmail.com>
2024-04-12 04:09:14 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+
+function findMatching(drivers, name) {
+    const matchingDrivers = [];
+    for (const driver of drivers) {
+      if (driver.toLowerCase() === name.toLowerCase()) {
+        matchingDrivers.push(driver);
+      }
+    }
+    return matchingDrivers;
+  }
+  
+  function fuzzyMatch(drivers, name) {
+    const matchingDrivers = [];
+    for (const driver of drivers) {
+      if (driver.toLowerCase().startsWith(name.toLowerCase())) {
+        matchingDrivers.push(driver);
+      }
+    }
+    return matchingDrivers;
+  }
+  
+  function matchName(drivers, name) {
+    const matchingDrivers = [];
+    for (const driver of drivers) {
+      if (driver.name.toLowerCase() === name.toLowerCase()) {
+        matchingDrivers.push(driver);
+      }
+    }
+    return matchingDrivers;
+  }
+  
+  
+  
+  
